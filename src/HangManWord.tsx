45b90d7a64ae7a3f53fc6cff0c9c367f8dc64358
LeaBani <113905453+LeaBani@users.noreply.github.com>
2023-04-22 01:27:50 +0200

    first commit

---
+++ src/HangManWord.tsx
@@ -0,0 +1,13 @@
+export function HangManWord() {
+    return <div style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize:"6rem",
+        fontWeight:"bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace"
+
+    }}>
+        
+    </div>
+}
